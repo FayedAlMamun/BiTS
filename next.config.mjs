a3b188abecb3bf4a8f934b9ea5c4b9a59d264e6d
FayeD <jit@Airbook.lan>
2025-11-08 03:42:47 +0600

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    qualities: [75, 85, 100], // ✅ add all quality values you use
+  },
+}
+
+export default nextConfig
